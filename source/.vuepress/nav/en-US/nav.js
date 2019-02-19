09531a24a16aafc8d4e9cb0934e156b42686d06e
InkoHX <inkohx@gmail.com>
2019-02-19 23:03:01 +0900

    Add MCBEForum link

--- source/.vuepress/nav/en-US/nav.js
+++ source/.vuepress/nav/en-US/nav.js
@@ -2,5 +2,9 @@ module.exports = [
     {
         text: 'Join Discord',
         link: 'https://discord.gg/U9SJAHU' 
+    },
+    {
+        text: 'MCBEForum',
+        link: 'https://forum.mcbe.jp'
     }
 ]
