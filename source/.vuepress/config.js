b184d2341bcc65ccd0f002583d9321c278b6efc2
InkoHX <me@inkohx.dev>
2022-02-01 01:17:47 +0900

    feat: VuePress v2.x 用に`config.js`をアップデート

--- source/.vuepress/config.js
+++ source/.vuepress/config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  lang: 'ja-JP',
+  title: 'server.mcbe.jp',
+  description: 'Minecraft:BedrockEdition(統合版)向けの、PocketMine-MP等のサーバーソフトウェアのプラグイン開発者向けコミュニティです。',
   head: [
-    ["meta", { charset: "utf-8" }],
     ["meta", { name: "twitter:card", content: "summary" }],
     ["meta", { name: "theme-color", content: "#3eaf7c" }],
     ["meta", { name: "og:title", content: "server.mcbe.jp" }],
@@ -8,7 +10,7 @@ module.exports = {
     ["meta", { name: "og:image", content: "https://server.mcbe.jp/icon.png" }],
     ["meta", { name: "og:type", content: "website" }],
     ["meta", { name: "og:locale", content: "ja_JP" }],
-    ["link", { rel: "icon", href: "https://server.mcbe.jp/icon.png" }]
+    ["link", { rel: "icon", href: "/icon.png" }]
   ],
   locales: {
     "/": {
@@ -24,6 +26,7 @@ module.exports = {
     }
   },
   themeConfig: {
+    logo: '/icon.png',
     repo: "PocketMineDevelopersJP/Guide",
     docsRepo: "PocketMineDevelopersJP/Guide",
     repoLabel: "GitHub",
@@ -32,22 +35,16 @@ module.exports = {
     editLinks: true,
     locales: {
       "/": {
-        label: "日本語",
-        selectText: "Languages",
+        selectLanguageName: "日本語",
+        selectLanguageText: "言語",
         editLinkText: "GitHubでこのページを編集",
-        lastUpdated: "最終更新日",
-        nav: require("./nav/ja-JP/nav"),
-        sidebar: {
-          "/guide/": require("./sidebar/community")("ガイド")
-        }
+        lastUpdatedText: "最終更新日",
+        contributorsText: '貢献者',
+        navbar: require("./nav/ja-JP/nav")
       },
       "/en-US/": {
-        label: "English",
-        selectText: "Languages",
-        editLinkText: "Edit this page on GitHub",
-        lastUpdated: "Last Updated",
-        nav: require("./nav/en-US/nav"),
-        sidebar: {}
+        selectLanguageName: 'English',
+        navbar: require("./nav/en-US/nav")
       }
     }
   }
